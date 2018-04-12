c8974600d54007f108adb4f42b6af05074be679e
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2018-04-12 13:36:53 -0400

    [githooks/checks/branch_is_company_specific.sh]  |  Add a hook to check that we aren't committing sensitive branches.

---
+++ githooks/checks/branch_is_company_specific.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+DEFAULT_ALLOWED_BRANCHES="master|dev"
+allowed_branches=$(cat "$(git rev-parse --show-toplevel)/allowed_branches")
+allowed_branches="${allowed_branches:-DEFAULT_ALLOWED_BRANCHES}"
+allowed_branches_regex="$(echo "$allowed_branches" | tr "\\n" '|' | sed 's/.$//')"
+$(git rev-parse --abbrev-ref HEAD | grep -Eq "$allowed_branches_regex")
