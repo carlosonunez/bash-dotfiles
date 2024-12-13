3239bf6979c906780c7d933461d1620f264e092b
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2024-12-12 22:43:43 -0600

    more reliable remote exec during testing
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- stacks/marcus/tests/helpers/faux_marcus.bash
+++ stacks/marcus/tests/helpers/faux_marcus.bash
@@ -3,14 +3,16 @@ run_in_faux_marcus() {
   _ensure_env_configured || exit 1
   _ensure_private_key_present || exit 1
 
-  ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /secrets/private_key \
+  ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /secrets/private_key \
     -p "$FAUX_MARCUS_SSH_PORT" \
     "${FAUX_MARCUS_SSH_USER}@host.docker.internal" \
     "$@"
 }
 
 faux_marcus_docker_compose() {
-  run_in_faux_marcus /opt/bin/docker-compose -f "$MARCUS_STACK_REMOTE_FILE_LOCATION" "$@"
+  cmd=$(printf 'cd "%s"; /opt/bin/docker-compose' \
+    "$(dirname "$MARCUS_STACK_REMOTE_FILE_LOCATION")")
+  run_in_faux_marcus sh -s <<< "$cmd $*"
 }
 
 _ensure_env_configured() {
